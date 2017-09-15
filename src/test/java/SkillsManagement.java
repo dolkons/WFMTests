8ee5397ca32bc875d6d30d47a9d404e5c11d7246
dolkons <dolkons@gmail.com>
2017-09-15 13:01:06 +0300

    Creating project

---
+++ src/test/java/SkillsManagement.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
+public class SkillsManagement {
+
+    private ChromeDriver chromeDriver;
+
+    @BeforeClass
+    public void setup(){
+
+    }
+
+}
